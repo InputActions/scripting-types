f805a3034c37c23037b7eeccfb9db9a3c2e6826c
taj-ny <79316397+taj-ny@users.noreply.github.com>
2026-06-14 11:24:50 +0200

    initial commit

---
+++ src/desktop/generic.d.ts
@@ -0,0 +1,57 @@
+/*
+    TypeScript type declarations for the InputActions scripting API
+    Copyright (C) 2026 Marcin Woźniak
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+export enum CursorShape {
+    Default,
+    ContextMenu,
+    Help,
+    Pointer,
+    Progress,
+    Wait,
+    Cell,
+    Crosshair,
+    Text,
+    VerticalText,
+    Alias,
+    Copy,
+    Move,
+    NoDrop,
+    NotAllowed,
+    Grab,
+    Grabbing,
+    EResize,
+    NResize,
+    NEResize,
+    NWResize,
+    SResize,
+    SEResize,
+    SWResize,
+    WResize,
+    EWResize,
+    NSResize,
+    NESWResize,
+    NWSEResize,
+    ColResize,
+    RowResize,
+    AllScroll,
+    ZoomIn,
+    ZoomOut,
+    DndAsk,
+    AllResize,
+    UpArrow,
+}
